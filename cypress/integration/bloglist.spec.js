8705549ad434568c80527ff8829e7478629c787c
kazumaneet <mekbibtare@gmail.com>
2022-01-09 11:19:36 +0300

    Wrote tests for deleteing blogs and tested them

--- cypress/integration/bloglist.spec.js
+++ cypress/integration/bloglist.spec.js
@@ -14,10 +14,6 @@ describe('Blog app', function () {
     cy.contains('login')
   })
   describe('Login', function () {
-    beforeEach(function () {
-
-    })
-
     it('succeeds with correct credentials', function () {
       cy.get('#username').type('kazuma')
       cy.get('#password').type('thePower')
@@ -38,7 +34,6 @@ describe('Blog app', function () {
       cy.get('#username').type('kazuma')
       cy.get('#password').type('thePower')
       cy.contains('login').click()
-
     })
 
     it('A blog can be created', function () {
@@ -50,6 +45,40 @@ describe('Blog app', function () {
       cy.get('#submit').click()
 
       cy.contains('Google')
+    })
+    describe('Deleting blogs', function () {
+      beforeEach(function () {
+        cy.contains('add blog').click()
+
+        cy.get('#author').type('Trevor')
+        cy.get('#title').type('Google')
+        cy.get('#url').type('https://www.google.com/')
+        cy.get('#submit').click()
+
+        cy.contains('Google')
+      })
+      it('User who created the blog can delete the blog', function () {
+        cy.contains('delete').click()
+        cy.contains('Google was deleted')
+      })
+      it.only('User who didnt create the blog can not delete the blog', function () {
+        const differentUser = {
+          name: 'another',
+          username: 'WaterMan',
+          password: 'theLover'
+        }
+        cy.request('POST', 'http://localhost:3003/api/users', differentUser)
+
+        cy.contains('Log Out').click()
+
+        cy.get('#username').type('WaterMan')
+        cy.get('#password').type('theLover')
+
+        cy.contains('login').click()
+
+        cy.contains('delete').click()
+        cy.contains('couldnt delete blog because')
+      })
     })
   })
 })
